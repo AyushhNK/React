668a12dd3b973d7aad10d6bd9bb56ea8ab9dcb8e
VINANK <neupaneayush3@gmail.com>
2024-02-05 15:02:05 +0545

    hello world

---
+++ helloworld/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Hello from './hello.jsx'
+
+// function App() {
+//   return (
+//     <h1>hello world</h1>
+//     )
+// }
+
+const App=()=> {
+  return (
+    <div>
+      <h1>hello i am a react component</h1>
+      <Hello />
+    </div>);
+};
+
+export default App;
+
+
---
+++ helloworld/src/hello.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Hello=()=>{
+	return <h1>Hell o world</h1>;
+}
+export default Hello;
