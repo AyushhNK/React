971da34b06431f6613c035e0df6aedf1fb838c32
VINANK <neupaneayush3@gmail.com>
2024-02-06 15:35:56 +0545

    finished tic-tac-toe

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Game from './tictac.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Game />
+    </>
+  )
+}
+
+export default App
