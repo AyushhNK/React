70e32560f7242c0f7813e52e71e360580e753f1c
VINANK <neupaneayush3@gmail.com>
2024-02-07 15:57:44 +0545

    finished todo list

---
+++ todolist/src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './App.css'; 
+
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="todo-container">
+      <h1 className="todo-header">Todo List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Add a new task"
+          className="task-input"
+        />
+        <button onClick={addTask} className="add-button">
+          Add
+        </button>
+      </div>
+      <ul className="task-list">
+        {tasks.map((task, index) => (
+          <li key={index} className="task-item">
+            {task}
+            <button onClick={() => removeTask(index)} className="remove-button">
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
