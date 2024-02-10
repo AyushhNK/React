81c3a30b6760b3a0312acefa4b238b05e24b5e09
VINANK <neupaneayush3@gmail.com>
2024-02-10 15:40:23 +0545

    added react router

--- helloworld/src/App.jsx
+++ helloworld/src/App.jsx
@@ -1,32 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import Hello from './hello.jsx'
-import Display from './user.jsx'
-import Condition from './conditonal.jsx'
-import List from './list.jsx'
-import MyButton from './events.jsx'
-import Count from './userstate.jsx'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Hello from './hello';
+import List from './list'
 
-// function App() {
-//   return (
-//     <h1>hello world</h1>
-//     )
-// }
-
-const App=()=> {
-  let cond=false;
+const App = () => {
   return (
-    <div>
-      <h1>hello i am a react component</h1>
-      <Hello />
-      <Display />
-      {cond && <Condition />}
-      <List />
-      <MyButton />
-      <Count />
-    </div>);
+    <Router>
+      <Routes>
+        <Route path="/hell" element={<Hello />} />
+        <Route path="/list" element={<List />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
-
 
