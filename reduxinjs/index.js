2b1d19568a4a941d2c1fec3b599cefbbeec05011
VINANK <neupaneayush3@gmail.com>
2024-04-03 18:07:55 +0545

    implemented increment and decrement

--- reduxinjs/index.js
+++ reduxinjs/index.js
@@ -1,48 +1,115 @@
 const redux=require('redux')
 const createStore=redux.createStore
 
-const CAKE_ORDERED = 'CAKE_ORDERED'
+//const CAKE_ORDERED = 'CAKE_ORDERED'
+//const CAKE_RESTOCKED='CAKE_RESTOCKED'
+//
+////Action creator
+//function orderCake() {
+//
+//    return {
+//        type: CAKE_ORDERED,
+//        quantity: 1
+//    }
+//}
+//
+//function restockCake(qty=1){
+//    return {
+//        type:CAKE_RESTOCKED,
+//        quantity:qty
+//    }
+//}
+//
+//const initialState={
+//    noOfCakes:10,
+//}
+//
+////Reducer
+//const reducer=(state=initialState,action)=>{
+//    switch(action.type){
+//        case CAKE_ORDERED:
+//            return {
+//                ...state,
+//                noOfCakes:state.noOfCakes-1,
+//            }
+//        case CAKE_RESTOCKED:
+//            return {
+//                ...state,
+//                noOfCakes:state.noOfCakes+action.quantity
+//            }
+//        default:
+//            return state
+//    }
+//    
+//}
+//
+////Store
+//const store=createStore(reducer)
+//console.log('Initial State',store.getState())
+//
+////subscribe to the store
+//const unsubscribe=store.subscribe(()=>console.log('updateState',store.getState()))
+//
+//
+////dispatching the action
+//store.dispatch(orderCake())
+//store.dispatch(orderCake())
+//store.dispatch(orderCake())
+//store.dispatch(restockCake(8))
+//
+////unsubscribing the store
+//unsubscribe()
 
-//Action creator
-function orderCake() {
+const INCREMENT="INCREMENT"
+const DECREMENT="DECREMENT"
 
+function incrementNumber() {
     return {
-        type: CAKE_ORDERED,
-        quantity: 1
+        type:INCREMENT,
+        payload:1
+    }
+}
+
+function decrementNumber(){
+    return {
+        type:DECREMENT,
+        payload:1
     }
 }
 
 const initialState={
-    noOfCakes:10,
+    value:0
 }
 
-//Reducer
 const reducer=(state=initialState,action)=>{
     switch(action.type){
-        case CAKE_ORDERED:
+        case INCREMENT:
             return {
                 ...state,
-                noOfCakes:state.noOfCakes-1,
+                value:state.value+1
+            }
+        case DECREMENT:
+            return {
+                ...state,
+                value:state.value-1
             }
         default:
             return state
     }
-    
 }
 
-//Store
 const store=createStore(reducer)
-console.log('Initial State',store.getState())
 
-//subscribe to the store
-const unsubscribe=store.subscribe(()=>console.log('updateState',store.getState()))
+console.log("initial state",store.getState())
 
+const unsubscribe=store.subscribe(()=>console.log("updatedState",store.getState()))
 
-//dispatching the action
-store.dispatch(orderCake())
-store.dispatch(orderCake())
-store.dispatch(orderCake())
+store.dispatch(incrementNumber())
+store.dispatch(incrementNumber())
+store.dispatch(incrementNumber())
+store.dispatch(incrementNumber())
+store.dispatch(decrementNumber())
+store.dispatch(decrementNumber())
+store.dispatch(decrementNumber())
 
-//unsubscribing the store
 unsubscribe()
-
